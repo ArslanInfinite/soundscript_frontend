6a3fbda03982deba43b365d78ed4ab05b8df5d4b
ArslanInfinite <ali.arslan319@gmail.com>
2021-02-20 03:35:07 -0500

    index.html and index.js connectivity and appending fetch info to DOM

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const endPoint = 'http://localhost:3000/api/v1/users'
+endPoint = 'http://localhost:3000/api/v1/users'
 
 document.addEventListener('DOMContentLoaded', () => {
-  fetch(endPoint)
-  .then(response => response.json())
-  .then(usersJson => {
-    console.log(usersJson)
-  })
+ getUsers();
 })
 
+function getUsers() {fetch(endPoint)
+  .then(response => response.json())
+  .then(users => {
+    users.data.forEach(user => {
+      const appendUser = `
+      <div data-id=${user.id}>
+        <h3>${user.attributes.name}</h3>
+      </div>
+      `
+      console.log(user)
+      document.querySelector('#users-container').innerHTML += appendUser
+    });
+  })
+}
